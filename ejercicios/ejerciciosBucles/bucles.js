1d8d5765e15a1e1a4d772fe12af000c21776f263
JChoqe <jchoque98c@gmail.com>
2025-10-30 13:28:51 +0100

    inicioEjercicioBucles

--- ejercicios/ejerciciosBucles/bucles.js
+++ ejercicios/ejerciciosBucles/bucles.js
@@ -4,3 +4,19 @@ import { Meses, diasSemana, diaDeLaSemana } from '../../data/datos.js';
 console.log(Meses);
 console.log(diasSemana);
 console.log(diaDeLaSemana('2025-10-30'));
+
+
+const form = document.getElementById('formAnio');
+const resultado = document.getElementById('resultado');
+const anio = document.getElementById('anio');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const anio = document.getElementById('anio').value;
+  mostrarAnio(anio);
+});
+
+function mostrarAnio(anio) {
+  resultado.textContent = `Año: ${anio}`; //Inprimir el año en html
+  console.log("Año recibido en bucles.js:", anio);
+}
