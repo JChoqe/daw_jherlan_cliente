bb3b61b0d614de6146f9d89250782166d0a5d075
JChoqe <jchoque98c@gmail.com>
2025-11-20 14:54:59 +0100

    din_por hoy

--- sesion3/ejercicioFinal/scripts/router.js
+++ sesion3/ejercicioFinal/scripts/router.js
@@ -1,3 +1,14 @@
 import { crearHeader, crearFooter } from './components.js';
 import { cargarPersonajesLS } from './api.js';
 import { pintarGrid } from './grid.js';
+
+const app = document.getElementById('app');
+
+async function iniciarApp() {
+    app.innerHTML = '';                    // limpiamos
+    app.appendChild(crearHeader());
+    app.appendChild(crearFooter());
+}
+
+// Arrancamos todo
+iniciarApp();
