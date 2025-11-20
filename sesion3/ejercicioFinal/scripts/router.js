4b3eb990ef51996763ba7418497e269b82f92a5d
JChoqe <jchoque98c@gmail.com>
2025-11-20 14:57:19 +0100

    fin

--- sesion3/ejercicioFinal/scripts/router.js
+++ sesion3/ejercicioFinal/scripts/router.js
@@ -7,6 +7,26 @@ const app = document.getElementById('app');
 async function iniciarApp() {
     app.innerHTML = '';                    // limpiamos
     app.appendChild(crearHeader());
+    const urlActual = window.location.href;   // la URL completa que estás viendo
+
+    // 1. ¿Estamos en la ficha de un personaje? (ej: ?id=5)
+    if (urlActual.includes("/articulo") && urlActual.includes("?id=")) {
+        // Sacamos el número del id (ej: de "?id=5" → sacamos el 5)
+        const id = urlActual.split("?id=")[1];
+        pintarFicha(id);                       // pintamos la ficha
+    }
+
+    // 2. ¿Estamos en la página del listado / tabla?
+    else if (urlActual.includes("/listado")) {
+        pintarTabla();                         // pintamos la tabla
+    }
+
+    // 3. Si no es ninguna de las dos → estamos en la página principal
+    else {
+        pintarGrid();                          // pintamos el grid 4×5
+    }
+
+
     app.appendChild(crearFooter());
 }
 
